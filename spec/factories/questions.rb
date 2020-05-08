ee76a459684ef24145f05319e47b2bb2081fafee
Tanya Pushchinska <tanya.pushchinska.com>
2020-05-08 17:54:42 +0300

    Added features specs

---
+++ spec/factories/questions.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :question do
+    title { 'MyString' }
+    body { 'MyText' }
+  end
+
+  factory :invalid_question, class: "Question" do
+    title { nil }
+    body { nil }
+  end
+end
