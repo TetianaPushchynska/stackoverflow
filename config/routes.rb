6a810c90dd8a97122d2a3faa603edab43dde3fd2
Tanya Pushchinska <tanya.pushchinska.com>
2020-05-18 12:09:23 +0300

    Added comments for questions and answers

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  concern :commentable do
+    resources :comments
+  end
+
   root "questions#index"
-  resources :questions do
+  resources :questions, concerns: :commentable do
     resources :answers
   end
+
+  resources :answers, concerns: :commentable
 end
